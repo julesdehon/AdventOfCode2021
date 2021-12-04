270f43203ca35f245a6e1df2cb21e1fbf52bd78b
Dehon, Jules <jules.dehon19@imperial.ac.uk>
2021-12-04 17:21:48 +0000

    Implement Day2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,20 +1,19 @@
 use std::fs;
-use std::str::{FromStr, Split};
+use std::str::FromStr;
 
 fn main() {
-    let contents = fs::read_to_string("src/input.txt")
+    let contents = fs::read_to_string("input.txt")
         .expect("Something went wrong reading the input file");
-    let lines = contents.split("\n");
-    let lines2 = contents.split("\n");
+    let lines = contents.split("\n").collect();
 
-    let increased_times = part1(lines);
-    let increased_sliding_window_times = part2(lines2);
+    let increased_times = part1(&lines);
+    let increased_sliding_window_times = part2(&lines);
 
     println!("Increased {} times", increased_times);
     println!("Sliding window increased {} times", increased_sliding_window_times);
 }
 
-fn part1(lines: Split<&str>) -> i32 {
+fn part1(lines: &Vec<&str>) -> i32 {
     let mut increased_times = 0;
     let mut prev = i32::MAX;
     for line in lines {
@@ -27,8 +26,8 @@ fn part1(lines: Split<&str>) -> i32 {
     return increased_times;
 }
 
-fn part2(lines: Split<&str>) -> i32 {
-    let numbered_lines: Vec<i32> = lines.map(|x| FromStr::from_str(x).unwrap()).collect();
+fn part2(lines: &Vec<&str>) -> i32 {
+    let numbered_lines: Vec<i32> = lines.into_iter().map(|x| FromStr::from_str(x).unwrap()).collect();
     let mut increased_times = 0;
     let mut prev = numbered_lines[0] + numbered_lines[1] + numbered_lines[2];
     for i in 3..numbered_lines.len() {
---
+++ day2/src/main.rs
@@ -0,0 +1,61 @@
+use std::fs;
+use std::str::FromStr;
+
+enum Command {
+    Forward(i32),
+    Down(i32),
+    Up(i32)
+}
+
+impl Command {
+    fn parse(raw: &str) -> Result<Command, &'static str> {
+        let split: Vec<&str> = raw.split(" ").collect();
+        return match split[0] {
+            "forward" => Ok(Command::Forward(FromStr::from_str(split[1]).unwrap())),
+            "down" => Ok(Command::Down(FromStr::from_str(split[1]).unwrap())),
+            "up" => Ok(Command::Up(FromStr::from_str(split[1]).unwrap())),
+            _ => Err("Could not match command")
+        }
+    }
+}
+
+fn main() {
+    let contents = fs::read_to_string("input.txt")
+        .expect("Something went wrong reading the input file");
+    let lines = contents.split("\n");
+    let commands: Vec<Command> = lines.map(|line| Command::parse(line).unwrap()).collect();
+
+    let (pos, depth) = part1(&commands);
+    println!("Reached position {}, and depth {} - multiplying gives {}", pos, depth, pos * depth);
+
+    println!("Using new interpretation of commands...");
+    let (pos, depth) = part2(&commands);
+    println!("Reached position {}, and depth {} - multiplying gives {}", pos, depth, pos * depth);
+}
+
+fn part1(commands: &Vec<Command>) -> (i32, i32) {
+    let mut pos: i32 = 0;
+    let mut depth: i32 = 0;
+    for command in commands {
+        match command {
+            Command::Forward(i) => { pos += i }
+            Command::Down(i) => { depth += i }
+            Command::Up(i) => { depth -= i }
+        }
+    }
+    return (pos, depth)
+}
+
+fn part2(commands: &Vec<Command>) -> (i32, i32) {
+    let mut pos: i32 = 0;
+    let mut depth: i32 = 0;
+    let mut aim: i32 = 0;
+    for command in commands {
+        match command {
+            Command::Forward(i) => { pos += i; depth += aim * i }
+            Command::Down(i) => { aim += i }
+            Command::Up(i) => { aim -= i }
+        }
+    }
+    return (pos, depth)
+}
