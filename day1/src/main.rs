57f9345c6361a9b027d0875f58fff103d04e631a
Dehon, Jules <jules.dehon19@imperial.ac.uk>
2021-12-04 14:48:19 +0000

    Implement second part of day1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,11 +1,20 @@
 use std::fs;
-use std::str::FromStr;
+use std::str::{FromStr, Split};
 
 fn main() {
     let contents = fs::read_to_string("src/input.txt")
         .expect("Something went wrong reading the input file");
     let lines = contents.split("\n");
+    let lines2 = contents.split("\n");
 
+    let increased_times = part1(lines);
+    let increased_sliding_window_times = part2(lines2);
+
+    println!("Increased {} times", increased_times);
+    println!("Sliding window increased {} times", increased_sliding_window_times);
+}
+
+fn part1(lines: Split<&str>) -> i32 {
     let mut increased_times = 0;
     let mut prev = i32::MAX;
     for line in lines {
@@ -15,6 +24,19 @@ fn main() {
         }
         prev = curr;
     }
+    return increased_times;
+}
 
-    println!("Increased {} times", increased_times);
+fn part2(lines: Split<&str>) -> i32 {
+    let numbered_lines: Vec<i32> = lines.map(|x| FromStr::from_str(x).unwrap()).collect();
+    let mut increased_times = 0;
+    let mut prev = numbered_lines[0] + numbered_lines[1] + numbered_lines[2];
+    for i in 3..numbered_lines.len() {
+        let curr : i32 = prev - numbered_lines[i - 3] + numbered_lines[i];
+        if curr > prev {
+            increased_times += 1;
+        }
+        prev = curr;
+    }
+    return increased_times;
 }
