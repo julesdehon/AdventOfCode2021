d2a12117b8dfe7847d7a0ede0c3eaeb7ffb21279
Dehon, Jules <jules.dehon19@imperial.ac.uk>
2021-12-05 15:31:57 +0000

    Implement day 3

---
+++ day3/src/main.rs
@@ -0,0 +1,68 @@
+use std::fs;
+
+fn main() {
+    let contents = fs::read_to_string("input.txt")
+        .expect("Something went wrong reading the input file");
+    let lines = contents.split("\n").collect();
+
+    let (gamma_rate, epsilon_rate) = part1(&lines);
+    println!("Gamma rate was {}, epsilon rate was {} - multiplying gives {}",
+             gamma_rate, epsilon_rate, gamma_rate * epsilon_rate);
+
+    let (oxygen_generator_rating, co2_scrubber_rating) = part2(&lines);
+    println!("Oxygen generator rating was {}, CO2 scrubber rating was {} - multiplying gives {}",
+             oxygen_generator_rating, co2_scrubber_rating, oxygen_generator_rating * co2_scrubber_rating)
+}
+
+fn part1(lines: &Vec<&str>) -> (i32, i32) {
+    let mut gamma_rate = "".to_owned();
+    let mut epsilon_rate = "".to_owned();
+    for i in 0..lines[0].len() {
+        let bits = lines.iter().map(|line| line.chars().nth(i).unwrap()).collect();
+        let mcb = most_common_bit(bits, '1');
+        gamma_rate.push(mcb);
+        epsilon_rate.push(if mcb == '1' { '0' } else { '1' });
+    }
+    return (i32::from_str_radix(gamma_rate.as_str(), 2).unwrap(),
+            i32::from_str_radix(epsilon_rate.as_str(), 2).unwrap());
+}
+
+fn part2(lines: &Vec<&str>) -> (i32, i32) {
+    let mut filtered_oxygen = lines.clone();
+    let mut curr_bit_pos = 0;
+    while filtered_oxygen.len() > 1 {
+        let oxygen_bits = filtered_oxygen.iter().map(|bit_string| bit_string.chars().nth(curr_bit_pos).unwrap()).collect();
+        let mcb = most_common_bit(oxygen_bits, '1');
+        if filtered_oxygen.len() > 1 {
+            filtered_oxygen.retain(|bit_string| bit_string.chars().nth(curr_bit_pos).unwrap() == mcb);
+        }
+        curr_bit_pos += 1;
+    }
+
+    let mut filtered_co2 = lines.clone();
+    curr_bit_pos = 0;
+    while filtered_co2.len() > 1 {
+        let co2_bits = filtered_co2.iter().map(|bit_string| bit_string.chars().nth(curr_bit_pos).unwrap()).collect();
+        let mcb = most_common_bit(co2_bits, '1'); // Set default to 1 since we are interested in least common bit
+        if filtered_co2.len() > 1 {
+            filtered_co2.retain(|bit_string| bit_string.chars().nth(curr_bit_pos).unwrap() == if mcb == '1' { '0' } else { '1' });
+        }
+        curr_bit_pos += 1;
+    }
+
+    return (i32::from_str_radix(filtered_oxygen[0], 2).unwrap(),
+            i32::from_str_radix(filtered_co2[0], 2).unwrap());
+}
+
+fn most_common_bit(bits: Vec<char>, default: char) -> char {
+    let mut ones = 0;
+    let mut zeros = 0;
+    for bit in bits {
+        match bit {
+            '1' => { ones += 1 },
+            '0' => { zeros += 1 },
+            _ => { println!("Unexpected bit found") }
+        }
+    }
+    return if ones > zeros { '1' } else if zeros > ones { '0' } else { default };
+}
